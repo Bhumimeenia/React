cb050f34e28ab5095f41952647c74206433aa29d
Bhumi <bhumimeenia0@gmail.com>
2025-03-31 23:58:18 +0530

    try1

--- App.js
+++ App.js
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React element
-// React.createElement => ReactElement.JS (Object) => HTMLElement(render)
-const heading = React.createElement("h1", null, "Hello World");
-
-console.log(heading); 
-
-// JSX - HTMl-like or XML-like syntax
-// jsx(transpiled) -> before going to the js engine
-
-// JSX => React.createElement => ReactElement.JS (Object) => HTMLElement(render)
 const jsxHeading = <h1>Hello World from JSX</h1>;
-console.log(jsxHeading); 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading); 
 
+// React component
+// -class based
+// -function based
+
+// const Fn = () => true; 
+
+// const Fn1  = () => {
+//     return true;
+// }
+
+// Root Functional Componenet
+const HeadingComponent = () => {
+  return <h1>Hello World from React Component</h1>;
+}; 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading); 
+root.render(<HeadingComponent/>); 
 
